82f5fec892eb126736c999eec45e29e95df7f488
ejioguvictor <ejioguvictor6054@gmail.com>
2022-04-25 22:43:11 +0100

    started spotify app clone with react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Login from './components/Login/Login';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>spotify</h1> */}
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "./Login.module.scss"
+
+function Login() {
+  return (
+    <div className={styles.login}>
+      <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
+
+      <a>LOGIN TO SPOTIFY HERE</a>
+    </div>
+  )
+}
+
+export default Login
