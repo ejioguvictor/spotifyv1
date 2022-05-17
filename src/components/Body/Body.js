c664bbbf5124a6fd94298fdb0695843082cb1e67
ejioguvictor <ejioguvictor6054@gmail.com>
2022-05-17 07:32:41 +0100

    ft: worked on the body component to display weekly songs

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import Header from '../Header/Header'
 import styles from "./Body.module.scss"
+import { useStateValue } from "../../DataLayer"
 
-function Body() {
+function Body({ spotify }) {
+  const [{ discover_weekly }, dispatch] = useStateValue()
+
   return (
     <div className={styles.body}>
-      <Header />
+      <Header spotify={spotify} />
+      <div className={styles["body_info"]}>
+        <img
+          src="https://images.unsplash.com/photo-1619983081593-e2ba5b543168?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+          alt="" />
+
+        <div className={styles["body_infoText"]}>
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
     </div>
   )
 }
